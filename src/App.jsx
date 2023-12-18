1e4ea347de4bb5ce45779d7c9d0fb61e19683175
Caro Valverde <carolina.valverde255@gmail.com>
2023-12-18 17:34:33 -0300

    agregamos la libreria de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-  
+//aqui agregamos la logica del componente
+ 
   return (
-  <h1>Prueba</h1>  
+//qui puedo agregar un poquito mas de logica
+    <h1 className="text-center display-3">
+      {/* agrego boostrap con npm i bootstrap@5.3.2 */}
+      {/* aqui agrego todo el maquetado del componente */}
+      Prueba</h1>  
   )
 }
 

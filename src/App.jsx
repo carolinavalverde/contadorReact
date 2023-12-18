b1a217b7e107d42a7ed3822ef8a01770e43fc1e3
Caro Valverde <carolina.valverde255@gmail.com>
2023-12-18 17:55:47 -0300

    agregamos el maquetado del componente app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-//aqui agregamos la logica del componente
- 
+  //aqui agregamos la logica del componente
+
   return (
-//qui puedo agregar un poquito mas de logica
-    <h1 className="text-center display-3">
-      {/* agrego boostrap con npm i bootstrap@5.3.2 */}
-      {/* aqui agrego todo el maquetado del componente */}
-      Prueba</h1>  
-  )
+    //qui puedo agregar un poquito mas de logica
+    <section className="container my-4">
+      <h1 className="text-center display-3">
+        {/* agrego boostrap con npm i bootstrap@5.3.2 */}
+        {/* aqui agrego todo el maquetado del componente */}
+        Contador
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nisi
+        tenetur deleniti, dolorum ab dolore modi id. Sed, culpa dolorum?
+      </p>
+    </section>
+  );
 }
 
-export default App
+export default App;
